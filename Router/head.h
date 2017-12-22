7e2a49eae44d847dd19e92de66238b0e0d25e601
firo <firo.y@outlook.com>
2017-12-22 14:29:13 +0800

    第一次提交

--- Router/head.h
+++ Router/head.h
@@ -1,7 +1,53 @@
 #pragma once
-#include<iostream>
+#include<string>
 
-int main()
+using namespace std;
+
+
+struct Vertex
 {
+	int numRouter;
+	string nameRouter;
+	
+	Edge *adj;
+};
 
-}
+struct Edge
+{
+	int dest;
+	string netNum;
+	int cost;
+
+	Edge *link;
+	Edge() {}
+	Edge(int i_dest, string s_netNum, int i_cost)
+	{
+		dest = i_dest;
+		netNum = s_netNum;
+		cost = i_cost;
+	}
+	bool operator!=(const Edge& t)const
+	{
+		return (dest != t.dest) ? true : false;
+	}
+};
+
+class Graph
+{
+public:
+	Graph();
+	~Graph();
+	
+protected:
+	Vertex *NodeTable;
+	int getVertexPos(const int vertex)
+	{
+		for (int i = 0; i < numVertices; i++)
+			if (NodeTable[i].numRouter == vertex)
+				return i;
+		return -1;
+	}
+
+	int numVertices;
+	int numEdges;
+};
